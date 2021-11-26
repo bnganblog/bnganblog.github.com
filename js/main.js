78a81056d18e06ac948e97de3e7bb093021575be
bnganblog <bnganblog@gmail.com>
2021-11-26 23:35:50 +0800

    Site updated: 2021-11-26 23:35:50

--- js/main.js
+++ js/main.js
@@ -134,4 +134,8 @@
         $mask.on('click', toggleToc);
         $('.navbar-main .catalogue').on('click', toggleToc);
     }
+    
+    $('div.container div.card[data-type=tags]').addClass('is-hidden-mobile');
+    $('div.container div.card[data-type=archives]').addClass('is-hidden-mobile');
+
 }(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
